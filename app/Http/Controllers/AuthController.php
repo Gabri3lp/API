f196481b7d70714eddd11a95d23dc8345945bff0
Unknown <Gabriel.jp215@gmail.com>
2018-08-21 11:58:27 -0300

    Acomodado el API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,12 +12,13 @@ class AuthController extends Controller
 {
     public function login(Request $request){
 	    $credentials = $request->only('email', 'password');
+	    $token = JWTAuth::attempt($credentials);
 	    if ( ! $token = JWTAuth::attempt($credentials)) {
 	            return response([
 	                'status' => 'error',
 	                'error' => 'invalid.credentials',
-	                'msg' => 'Invalid Credentials.'
-	            ], 400);
+	                'message' => 'Invalid Credentials'
+	            ]);
 	    }
 	    return response([
 	            'status' => 'success',
